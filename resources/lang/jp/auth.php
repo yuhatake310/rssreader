29bfe37e19dc43c403e84b76deef5571d7c2c3f8
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2020-01-05 01:47:43 +0900

    ログイン機能の実装

---
+++ resources/lang/jp/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'メールアドレスまたはパスワードに誤りがあります。',
+    'throttle' => 'ログイン回数が多すぎます。 :second 秒後にもう一度お試しください。',
+
+];
